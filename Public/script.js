8ef6634930d40ca21cee50ed116f8f4506d57152
DiogoLuz <diogolinoluz@hollandandbarrett.com>
2022-10-26 16:30:57 +0100

    Creating static functions and cleaning up code

--- Public/script.js
+++ Public/script.js
@@ -1,25 +1,24 @@
-const JamCamEndpoint = "https://api.tfl.gov.uk/Place/Type/JamCam"
+const JamCamEndpoint = "https://api.tfl.gov.uk/Place/Type/JamCam";
 
-class App{
-    constructor(){
-
-    }
-    HTTPGet(Address){
-        return fetch(Address,{
-            method:"get",
-            headers:{
-                "Cache-Control":"no-cache"
-            }
-        })
-    }
-    JamCamRequest(){
-        return this.HTTPGet(JamCamEndpoint).then(result=>{return(result.json())}) 
-    }
-    async ListJamCam(){
-       await this.JamCamRequest().then(result=>{console.log(result)})
-    }
+class App {
+  static HTTPGet(Address) {
+    return fetch(Address, {
+      method: "get",
+      headers: {
+        "Cache-Control": "no-cache",
+      },
+    });
+  }
+  static JamCamRequest() {
+    return App.HTTPGet(JamCamEndpoint).then((result) => {
+      return result.json();
+    });
+  }
+  static async ListJamCam() {
+    await App.JamCamRequest().then((result) => {
+      console.log(result);
+    });
+  }
 }
 
-let CurrentInstance = new App
-
-CurrentInstance.ListJamCam()
+App.ListJamCam();
