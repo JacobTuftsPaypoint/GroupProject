961efb3a9a32c0394862534fff256e9a25f63f4f
DiogoLuz <diogolinoluz@hollandandbarrett.com>
2022-10-27 10:18:03 +0100

    Changed Asset class to a more fitting name: JamCam

--- Public/script.js
+++ Public/script.js
@@ -1,86 +1,78 @@
 const JamCamEndpoint = "https://api.tfl.gov.uk/Place/Type/JamCam";
 
-const Cameras = []
+const Cameras = [];
 
-class Asset{
-    constructor(TempObj){
-        this.name = TempObj.commonName
-        this.lat = TempObj.lat
-        this.lon = TempObj.lon
-        this.ImageUrl = TempObj.additionalProperties[1].value
-        this.VideoUrl = TempObj.additionalProperties[2].value
-        this.status = TempObj.additionalProperties[0].value
+class JamCam {
+  constructor(TempObj) {
+    this.name = TempObj.commonName;
+    this.lat = TempObj.lat;
+    this.lon = TempObj.lon;
+    this.ImageUrl = TempObj.additionalProperties[1].value;
+    this.VideoUrl = TempObj.additionalProperties[2].value;
+    this.status = TempObj.additionalProperties[0].value;
+  }
+  CreateTile(Container) {
+    let tile = document.createElement("section");
+    let name = document.createElement("h4");
+    let latp = document.createElement("p");
+    let lonp = document.createElement("p");
+    let img = document.createElement("img");
+
+    if (this.status == "true") {
+      tile.style.backgroundColor = "green";
+    } else {
+      tile.style.backgroundColor = "red";
     }
-    CreateTile(Container){
-        let tile = document.createElement("section")
-        let name = document.createElement("h4")
-        let latp = document.createElement("p")
-        let lonp = document.createElement("p")
-        let img = document.createElement("img")
 
-        if (this.status=="true") {
-            tile.style.backgroundColor = "green"
-        } else {
-            tile.style.backgroundColor = "red"
-        }
+    name.innerText = this.name;
+    latp.innerText = this.lat;
+    lonp.innerText = this.lon;
+    img.setAttribute("src", this.ImageUrl);
 
-        name.innerText=this.name
-        latp.innerText=this.lat
-        lonp.innerText=this.lon
-        img.setAttribute("src",this.ImageUrl) 
+    tile.appendChild(name);
+    tile.appendChild(latp);
+    tile.appendChild(lonp);
+    tile.appendChild(img);
 
-        tile.appendChild(name)
-        tile.appendChild(latp)
-        tile.appendChild(lonp)
-        tile.appendChild(img)
-
-        Container.appendChild(tile)
-    }
+    Container.appendChild(tile);
+  }
 }
 
-class App{
-    constructor(){
+class App {
+  constructor() {}
+  HTTPGet(Address) {
+    return fetch(Address, {
+      method: "get",
+      headers: {
+        "Cache-Control": "no-cache",
+      },
+    });
+  }
+  JamCamRequest() {
+    return this.HTTPGet(JamCamEndpoint).then((result) => {
+      return result.json();
+    });
+  }
+  async ListJamCam() {
+    await this.JamCamRequest().then((result) => {
+      result.forEach((element) => {
+        Cameras.push(new Asset(element));
+      });
+    });
+  }
+  static async getSpecificJamCam(location) {
+    await App.JamCamRequest().then((result) => {
+      for (const camera of result) {
+        if (camera.commonName === location) {
+          console.log("Found Camera!");
 
-    }
-    static HTTPGet(Address){
-        return fetch(Address,{
-            method:"get",
-            headers:{
-                "Cache-Control":"no-cache"
-            }
-        })
-    }
+          console.log(camera);
 
-    static async JamCamRequest(){
-        return App.HTTPGet(JamCamEndpoint).then(result=>{return(result.json())}) 
-    }
-
-    static async ListJamCam(){
-       await App.JamCamRequest().then(result=>{
-        result.forEach(element => {
-            Cameras.push(new Asset(element))
-        });
-       })
-    }
-
-    static async getSpecificJamCam(location) {
-        await App.JamCamRequest().then((result) => {
-            for (const camera of result) {
-                if (camera.commonName === location) {
-                    console.log("Found Camera!");
-                    console.log(camera);
-                    break;
-                }
-            }
-        });
-    }    
+          break;
+        }
+      }
+    });
+  }
 }
 
 App.getSpecificJamCam("A3 West Hill/Up Richmond Rd");
-
-App.ListJamCam().then(()=>{
-    for (let i = 0; i < Cameras.length; i++) {
-        const element = Cameras[i];
-        element.CreateTile(document.querySelector("body"))
-    }
-})
